8fd09561ae00647e8691394de4046ca8236cfc79
김영주 <kyle2123@naver.com>
2021-07-22 02:25:05 +0900

    feat: 방 설정 화면 애니메이션 적용
    - 3초의 시간 지연을 통해 동적인 효과 주입
    
    Closes S05P12A104-52

--- frontend/src/views/room-setting/Room-setting.vue
+++ frontend/src/views/room-setting/Room-setting.vue
@@ -7,18 +7,31 @@
         alt="mafia logo image"
       />
     </div>
-    <GameMode />
+    <GameMode id="gamemode" v-show="isShow" />
   </div>
 </template>
 
 <script>
 import "./room-setting.css";
-import GameMode from '@/views/room-setting/components/GameMode.vue'
+import GameMode from "@/views/room-setting/components/GameMode.vue";
+import {onMounted, ref} from 'vue';
 
 export default {
-  name: 'Room-setting',
+  name: "Room-setting",
   components: {
     GameMode,
+  },
+  setup() {
+    let isShow = ref(false)
+    onMounted(() => {
+      // Mafia logo 올라가고 라디오 버튼이 나와야하므로
+      setTimeout(() => {
+        isShow.value = true
+      }, 3300);
+    })
+    return {
+      isShow,
+    }
   }
 };
 </script>
