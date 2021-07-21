13d9e13ad5d774f44eb5d03fd2391e496a7f92bb
김영주 <kyle2123@naver.com>
2021-07-21 23:12:20 +0900

    feat: 방만들기 버튼 추가 및 CSS 적용
    - token이 없으면 방만들기 버튼 클릭시 경고창
    - Mafia 배경 이미지 설정, Element Plus 적용
    
    Closes S05P12A104-31

--- frontend/src/views/home/components/Home.vue
+++ frontend/src/views/home/components/Home.vue
@@ -1,51 +1,83 @@
 <template>
-    <img alt="Vue logo" src="@/assets/logo.png">
-    <login></login> 
+  <div class="wrap">
+    <div>
+      <img class="bg-image" src="@/assets/image/mafia.png" alt="mafia image" />
+    </div>
+    <div class="my-4">
+      <el-button type="primary" size="medium" @click="moveToNickname">
+        방 만들기
+      </el-button>
+    </div>
+    <Login />
+  </div>
 </template>
 
 <script>
-import Login from '@/views/home/components/Login.vue'
+import Login from "@/views/home/components/Login.vue";
+import { ElMessage } from "element-plus";
 // import { onMounted } from '@vue/runtime-core'
 // import {useStore} from 'vuex';
 
 export default {
-    name: 'Home',
-    components: {
-        Login,
-    },
-    created(){
-            const Token = localStorage.getItem("token");
-            console.log('Token',Token);
-            if(Token){
-                this.$store.dispatch('token/setIsLogin', true);
-                this.$store.dispatch('token/setToken', Token);
-            }else{
-                this.$store.dispatch('token/setIsLogin', false);
-            }
-    },
-    // setup(){
-    //     const store = useStore();
-        
-    //     onMounted(() => {
-    //         const Token = localStorage.getItem("token");
-    //         console.log('Token',Token);
-    //         if(Token){
-    //             store.dispatch('token/setIsLogin', true);
-    //             store.dispatch('token/setToken', Token);
-    //         }else{
-    //             store.dispatch('token/setIsLogin', false);
-    //         }
-    //     })
-    // }
-}
+  name: "Home",
+  components: {
+    Login,
+  },
+  created() {
+    const Token = localStorage.getItem("token");
+    console.log("Token", Token);
+    if (Token) {
+      this.$store.dispatch("token/setIsLogin", true);
+      this.$store.dispatch("token/setToken", Token);
+    } else {
+      this.$store.dispatch("token/setIsLogin", false);
+    }
+  },
+  setup(){
+    const moveToNickname = () => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        ElMessage({
+          message: "닉네임 설정으로 이동합니다!",
+          type: "success",
+        });
+      } else {
+        ElMessage.error("로그인이 필요합니다!");
+      }
+    };
+
+    return {
+      moveToNickname,
+    }
+    // const store = useStore();
+
+    // onMounted(() => {
+    //     const Token = localStorage.getItem("token");
+    //     console.log('Token',Token);
+    //     if(Token){
+    //         store.dispatch('token/setIsLogin', true);
+    //         store.dispatch('token/setToken', Token);
+    //     }else{
+    //         store.dispatch('token/setIsLogin', false);
+    //     }
+    // })
+  }
+};
 </script>
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.wrap {
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+}
+.bg-image {
+  width: 40%;
 }
 </style>
