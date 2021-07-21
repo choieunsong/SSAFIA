c5d2a4b63292a8b4a5dd6872c5df356c8cf0bb36
hoonti06 <hoonti06@gmail.com>
2021-07-21 16:23:19 +0900

    build : spring boot 프로젝트 생성

---
+++ backend/src/main/java/s05/p12a104/mafia/MafiaApplication.java
@@ -0,0 +1,13 @@
+package s05.p12a104.mafia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MafiaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MafiaApplication.class, args);
+  }
+
+}
