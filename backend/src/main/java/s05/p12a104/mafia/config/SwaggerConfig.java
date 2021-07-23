9db4dc0f49a63bb4254ed3aaab7631653dbb6d5d
adlguswjda <adlguswjda@naver.com>
2021-07-23 13:28:32 +0900

    perf: swagger api 문서 개선
    전체 설명과 controller 설명 추가

--- backend/src/main/java/s05/p12a104/mafia/config/SwaggerConfig.java
+++ backend/src/main/java/s05/p12a104/mafia/config/SwaggerConfig.java
@@ -4,8 +4,10 @@ import static com.google.common.collect.Lists.newArrayList;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.SecurityReference;
@@ -19,30 +21,34 @@ import springfox.documentation.spring.web.plugins.Docket;
 @Configuration
 public class SwaggerConfig {
 
-  public static final String SECURITY_SCHEMA_NAME = "JWT";
-  
+  public static final String SECURITY_NAME = "JWT";
+
   @Bean
   public Docket api() {
-    return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false).select()
-        .apis(RequestHandlerSelectors.any()).paths(PathSelectors.ant("/api/**")).build()
+    return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
+        .useDefaultResponseMessages(false).select().apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.ant("/api/**")).build()
         .securityContexts(newArrayList(securityContext())).securitySchemes(newArrayList(apiKey()));
   }
 
+  private ApiInfo apiInfo() {
+    String description = "mafia 프로젝트 api 문서";
+    return new ApiInfoBuilder().title("Mafia API").description(description).build();
+  }
+
   private ApiKey apiKey() {
-    return new ApiKey(SECURITY_SCHEMA_NAME, "Authorization", "header");
+    return new ApiKey(SECURITY_NAME, "Authorization", "header");
   }
 
   private SecurityContext securityContext() {
     return SecurityContext.builder().securityReferences(defaultAuth()).build();
   }
 
-
   private List<SecurityReference> defaultAuth() {
-    AuthorizationScope authorizationScope =
-        new AuthorizationScope("global", "accessEverything");
+    AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
     AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
     authorizationScopes[0] = authorizationScope;
-    return newArrayList(new SecurityReference(SECURITY_SCHEMA_NAME, authorizationScopes));
+    return newArrayList(new SecurityReference(SECURITY_NAME, authorizationScopes));
   }
 
 }
