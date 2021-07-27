5fa121e1773bcf709bb2daa9e7e4a4756aa36e2c
yhpgKIM <greenboy94@naver.com>
2021-07-27 16:09:45 +0900

    fix: room state option

--- backend/src/main/java/s05/p12a104/mafia/api/service/GameSessionServiceImpl.java
+++ backend/src/main/java/s05/p12a104/mafia/api/service/GameSessionServiceImpl.java
@@ -7,6 +7,7 @@ import s05.p12a104.mafia.api.requset.GameSessionPostReq;
 import s05.p12a104.mafia.common.exception.GameSessionException;
 import s05.p12a104.mafia.common.util.RandomRoomIdUtils;
 import s05.p12a104.mafia.domain.entity.GameSession;
+import s05.p12a104.mafia.domain.entity.GameState;
 import s05.p12a104.mafia.domain.entity.User;
 import s05.p12a104.mafia.domain.repository.GameSessionRedisRepository;
 
@@ -30,6 +31,7 @@ public class GameSessionServiceImpl implements GameSessionService {
     if (gameSessionRedisRepository.findByCreatorEmail(user.getEmail()).size() >= 2) {
       throw new GameSessionException("더 이상 방을 만들 수 없습니다");
     }
+
     gameSessionRedisRepository.save(
         new GameSession(id, user.getEmail(), typeInfo.getAccessType(), typeInfo.getRoomType()));
 
@@ -43,6 +45,15 @@ public class GameSessionServiceImpl implements GameSessionService {
   public GameSession enterGame(String roomId) {
     GameSession enter = gameSessionRedisRepository.findById(roomId)
         .orElseThrow(() -> new GameSessionException("방정보를 찾을 수 없습니다"));
+
+    if (enter.getPlayers().size() > 9) {
+      throw new GameSessionException("정원이 가득 찾습니다.");
+    }
+
+    if (enter.getState().equals(GameState.started)) {
+      throw new GameSessionException("게임이 이미 시작되었습니다.");
+    }
+
     return enter;
   }
 }
--- backend/src/main/java/s05/p12a104/mafia/domain/entity/GameState.java
+++ backend/src/main/java/s05/p12a104/mafia/domain/entity/GameState.java
@@ -1,7 +1,7 @@
 package s05.p12a104.mafia.domain.entity;
 
 public enum GameState {
-  ingame,
+  started,
   ready,
   wait
 }
