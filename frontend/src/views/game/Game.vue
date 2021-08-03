7a7839179904f087e76150dcbe860446e9296703
OTKRyu <appn12@gmail.com>
2021-08-03 18:04:59 +0900

    feat: 메인 메세지 및 직업별 메세지 어느정도 정해짐, 웹소켓 틀도 어느정도 정해짐

--- frontend/src/views/game/Game.vue
+++ frontend/src/views/game/Game.vue
@@ -57,20 +57,19 @@ export default {
 
       // socket 연결 게임 데이터 관련
       isHost: undefined,
-      Host: undefined,
       role: undefined,
       gameStatus: {
         date: 0,
         phase: "ready",
         timer: 0,
         aliveMafia: 0,
-        victim: undefined,
-        victimIsMafia: undefined,
-        suspects: undefined,
       },
-      players: undefined,
+      playerMap: undefined,
       stompClient: undefined,
-      jonClient: undefined,
+      jobClient: undefined,
+      mafias: undefined,
+      message: undefined,
+      submessage: '',
     });
 
     // 화상 채팅 관련
@@ -98,10 +97,10 @@ export default {
       // On every new Stream received...
       state.session.on("streamCreated", ({ stream }) => {
         const subscriber = state.session.subscribe(stream);
-        console.log("subscriber");
-        console.log(subscriber);
         const array = subscriber.stream.connection.data.split('"');
-        subscriber.playerId = array[3];
+        const tmp = array[3].split(",");
+        subscriber.nickname = tmp[0];
+        subscriber.playerId = tmp[1];
         state.subscribers.push(subscriber);
       });
 
@@ -120,7 +119,9 @@ export default {
 
       console.log(state.openviduToken);
       state.session
-        .connect(state.openviduToken, { clientData: state.playerId })
+        .connect(state.openviduToken, {
+          clientData: `${state.myUserName},${state.playerId}`,
+        })
         .then(() => {
           // --- Get your own camera stream with the desired properties ---
 
@@ -150,19 +151,24 @@ export default {
           );
         });
     };
+
     // 게임 관련 소켓통신
     function onConnected() {
       // Subscribe to the Public Topic
-      state.stompClient.subscribe(`/sub/all`, onMessageReceived);
+      state.message = `Room: ${state.mySessionId}에 오신 걸 환영합니다. \n 부디 SSAFIA를 즐겨주시기 바랍니다`;
       state.stompClient.subscribe(
-        `/sub/${state.playerId}`,
+        `/sub/${state.mySessionId}`,
+        onMessageReceived
+      );
+      state.stompClient.subscribe(
+        `/sub/${state.mySessionId}/${state.playerId}`,
         onPersonalMessageReceived
       );
       // Tell your username to the server
       state.stompClient.send(
-        "/pub",
+        `/pub/${state.mySessionId}`,
         {},
-        JSON.stringify({ playerId: state.playerId, type: "CONNECT" })
+        JSON.stringify({ id: state.playerId, type: "JOIN" })
       );
     }
 
@@ -171,29 +177,79 @@ export default {
     }
 
     function connect() {
-      var socket = new SockJS(`/ws/${state.mySessionId}`);
+      var socket = new SockJS("/ws/gamesession");
       state.stompClient = Stomp.over(socket);
       state.stompClient.connect({}, onConnected, onError);
     }
 
     function sendMessageVote(targetPlayerId) {
       if (state.stompClient) {
-        var Message = {
-          playerId: state.playerId,
+        const Message = {
+          id: state.playerId,
           vote: targetPlayerId,
-          type: "VOTE",
+          phase: state.gameStatus.phase,
         };
-        state.stompClient.send("/pub", {}, JSON.stringify(Message));
+        state.stompClient.send(
+          `/pub/${state.mySessionId}/vote`,
+          {},
+          JSON.stringify(Message)
+        );
       }
     }
 
     function sendMessageConfirm() {
-      if (state.stompCliesnt) {
-        var Message = {
-          playerId: state.playerId,
-          type: "CONFIRM",
+      if (state.stompClient) {
+        const Message = {
+          id: state.playerId,
+          phase: state.gameStatus.phase,
         };
-        state.stompClient.send("/pub", {}, JSON.stringify(Message));
+        state.stompClient.send(
+          `/pub/${state.mySessionId}/confirm`,
+          {},
+          JSON.stringify(Message)
+        );
+      }
+    }
+
+    function sendMessageStart() {
+      if (state.stompClient) {
+        const Message = {
+          id: state.playerId,
+        };
+        state.stompClient.send(
+          `/pub/${state.mySessionId}/start`,
+          {},
+          JSON.stringify(Message)
+        );
+      }
+    }
+
+    function sendMessageNightVote(targetPlayerId) {
+      if (state.stompClient) {
+        const message = {
+          phase = state.gameStatus.phase,
+          id = state.playerId,
+          vote = targetPlayerId
+        }
+        state.stompClient.send(
+          `/pub/${state.mySessionId}/${state.role}/vote`,
+          {},
+          JSON.stringify(message)
+        )
+      }
+    }
+
+    function sendMessageNightConfirm() {
+      if (state.stompClient) {
+        const message = {
+          phase = state.gameStatus.phase,
+          id = state.playerId,
+        }
+        state.stompClient.send(
+          `/pub/${state.mySessionId}/${state.role}/confirm`,
+          {},
+          JSON.stringify(message)
+        )
       }
     }
 
@@ -202,57 +258,218 @@ export default {
       if (message.type === "JOIN") {
         state.players = message.players;
       } else if (message.type === "LEAVE") {
-        state.players = message.players;
-      } else if (message.type === "HOSTCHANGED") {
-        state.host = message.host;
-        if (state.host === state.playerId) {
+        delete state.players[message.leftPlayerId];
+        if (message.hostId === state.playerId) {
           state.isHost = true;
         } else {
           state.isHost = false;
         }
-      } else if (message.type === "GAMESESSION") {
-        state.gameStatus = message.gameStatus;
-        state.players = message.players;
-        if (state.players[state.playerId].alive === true) {
-          switch (message.phase) {
-            case "START":
-              break;
-            case "DAYTALK":
-              break;
-            case "DAYTOKILL":
-              break;
-            case "DAYVOTEKILL":
-              break;
-            case "DAYTONIGHT":
-              break;
-            case "NIGHTVOTE":
-              break;
-            case "NIGHTTODAY":
-              break;
-            case "END":
-              state.jobClient.unsubscribe()
-              break;
-            default:
-              console.log(
-                `sorry, something go wrong, this is what i've got ${message.phase}`
-              );
-          }
+      } else if (message.type === "PHASE_CHANED") {
+        state.submessage = ''
+        switch (message.gameStatus.phase) {
+          case "START":
+            if (state.role === "mafia") {
+              state.message = `게임이 시작되었습니다. \n 당신은 마피아입니다. \n 마피아 동료와 함께 시민의 수를 마피아의 수와 같게 만들면 당신의 승리입니다. \n 밤마다 마피아 동료들과 상의해 시민을 한명씩 제거해나가세요. \n 당신의 마피아 동료는 ${state.mafias}들입니다`;
+            } else if (state.role === "police") {
+              state.message =
+                "게임이 시작되었습니다. \n 당신은 경찰입니다. \n 시민을 도와 마피아를 모두 제거하면 당신의 승리입니다. \n 밤마다 의심가는 사람 한 명을 지목하여 그 사람의 직업을 알아낼 수 있습니다.";
+            } else if (state.role === "doctor") {
+              state.message =
+                "게임이 시작되었습니다. \n 당신은 의사입니다. \n 시민을 도와 마피아를 모두 제거하면 당신의 승리입니다. \n 밤마다 죽을 것 같은 사람에게 투표하여 그 사람을 구할 수 있습니다.";
+            } else if (state.role === "civilian") {
+              state.message =
+                "게임이 시작되었습니다. \n 당신은 시민입니다. \n 다른 시민과 함께 마피아를 모두 제거하면 당신의 승리입니다.";
+            } else {
+              state.message =
+                "당신은 관전자입니다. \n 게임에 개입할 수는 없지만, 모든 종류의 일어나고 있는 일들에 대한 정보를 받아볼 수 있습니다.";
+            }
+            state.gameStatus = message.gameStatus;
+            const keys = Object.keys(state.playerMap);
+            for (let i = 0; i < keys.length; i++) {
+              const key = keys[i]; // 각각의 키
+              const value = state.playerMap[key]; // 각각의 키에 해당하는 각각의 값
+              state.playerMap[value].alive = message.playerMap[value].alive;
+              state.playerMap[value].voters = []
+            }
+            break;
+          case "DAY_DISCUSSION":
+            state.message =
+              "낮 투표시간이 되었습니다. \n 각자 의심되는 사람을 지목해 주세요. \n 최다 득표를 한 사람들은 최종투표에 나가게 됩니다.";
+            state.gameStatus = message.gameStatus;
+            const keys = Object.keys(state.playerMap);
+            for (let i = 0; i < keys.length; i++) {
+              const key = keys[i]; // 각각의 키
+              const value = state.playerMap[key]; // 각각의 키에 해당하는 각각의 값
+              state.playerMap[value].alive = message.playerMap[value].alive;
+              state.playerMap[value].voters = []
+            }
+            break;
+          case "DAY_ELIMINATION":
+            state.message =
+              "최종투표시간이 되었습니다. \n 최종투표 후보자들 중에 제거할 사람에게 투표해 주세요. \n 최다득표자는 제거되게 됩니다.";
+            state.gameStatus = message.gameStatus;
+            const keys = Object.keys(state.playerMap);
+            for (let i = 0; i < keys.length; i++) {
+              const key = keys[i]; // 각각의 키
+              const value = state.playerMap[key]; // 각각의 키에 해당하는 각각의 값
+              state.playerMap[value].alive = message.playerMap[value].alive;
+              state.playerMap[value].suspicious =
+                message.playerMap[value].suspicious;
+              state.playerMap[value].voters = []
+            }
+            break;
+          case "DAY_TO_NIGHT":
+            if (state.gaemStatus === "DAY_DISCUSSION") {
+              state.message =
+                "최다 득표자가 너무 많거나 또는 무효투표자가 너무 많은 관계로,\n  최종 투표를 스킵하고 밤으로 넘어갑니다.";
+            } else {
+              let victimNickname = ''
+              for (let i = 0; i < state.subscribers.length; i++) {
+                if (state.subscribers[i].playerId === message.victim) {
+                  victimNickname = state.subscribers[i].nickname
+                  break
+                }
+              }
+              const victimJob = victimIsMafia ? '마피아' : '시민'
+              state.message = `낮의 투표 결과로 인해, ${ victimNickname }님이 제거되었습니다. \n ${ victimNickname}님의 직업은 ${ victimJob }이였습니다 \n 곧 밤으로 넘어갑니다.`;
+            }
+            state.gameStatus = message.gameStatus;
+            const keys = Object.keys(state.playerMap);
+            for (let i = 0; i < keys.length; i++) {
+              const key = keys[i]; // 각각의 키
+              const value = state.playerMap[key]; // 각각의 키에 해당하는 각각의 값
+              state.playerMap[value].alive = message.playerMap[value].alive;
+              state.playerMap[value].voters = []
+            }
+            break;
+          case "NIGHT_VOTE":
+            if (state.role === 'mafia') {
+              state.message = '밤이 되었습니다. 마피아는 시민 중 제거할 사람을 투표하여 주시기 바랍니다.'
+            } else if (state.role === 'doctor' ) {
+              state.message = '밤이 되었습니다. 의사는 시민 중 제거당할 것 같은 사람에게 투표하여 주시기 바랍니다.'
+            } else if (state.role === 'police') {
+              state.message = '밤이 되었습니다. 경찰은 의심되는 사람을 지목하여 그 사람의 직업을 확인해보시기 바랍니다.'
+            } else if (state.role === 'civilian') {
+              state.message = '밤이 되었습니다. 마이크와 비디오가 중단됩니다.'
+            } else {
+              state.message = '밤이 되었습니다. 각 직업마다 뭘 하고 있는지 지켜보시길 바랍니다.'
+            }
+            state.gameStatus = message.gameStatus;
+            const keys = Object.keys(state.playerMap);
+            for (let i = 0; i < keys.length; i++) {
+              const key = keys[i]; // 각각의 키
+              const value = obj[key]; // 각각의 키에 해당하는 각각의 값
+              state.playerMap[value].alive = message.playerMap[value].alive;
+              state.playerMap[value].voters = []
+            }
+            break;
+          case "NIGHT_TO_DAY":
+            if (message.gameStatus.victim) {
+              let victimNickname = ''
+              for (let i = 0; i < state.subscribers.length; i++) {
+                if (state.subscribers[i].playerId === message.victim) {
+                  victimNickname = state.subscribers[i].nickname
+                  break
+                }
+              }
+              const victimJob = message.gameStatus.victimIsMafia ? '마피아' : '시민'
+              state.message = `밤의 투표 결과로 인해, ${ victimNickname }님이 제거되었습니다. \n ${ victimNickname}님의 직업은 ${ victimJob }이였습니다 \n 곧 밤으로 넘어갑니다.`;
+            } else {
+              state.message = "밤의 투표 결과, 아무도 죽지 않았습니다."
+            }
+            state.gameStatus = message.gameStatus;
+            const keys = Object.keys(state.playerMap);
+            for (let i = 0; i < keys.length; i++) {
+              const key = keys[i]; // 각각의 키
+              const value = obj[key]; // 각각의 키에 해당하는 각각의 값
+              state.playerMap[value].alive = message.playerMap[value].alive;
+              state.playerMap[value].voters = []
+            }
+            break;
+          case "END":
+            const winner = (message.gameStatus.winner === 'mafia') ? '마피아' : '시민'
+            state.message = `게임이 종료되었습니다. 최종승자는 ${message.gameStatus.winner}입니다.`
+
+            // 초기화
+            state.role = undefined
+            state.gameStatus = {
+              date: 0,
+              phase: "ready",
+              timer: 0,
+              aliveMafia: 0,
+            }
+            // state.playerMap = undefined
+            state.jobClient = undefineds
+            state.mafias = undefined
+            state.message = undefined
+            state.submessage = ''
+            break;
         }
+      } else if (message.type === "UPDATE") {
+        const keys = Object.keys(state.playerMap);
+        for (let i = 0; i < keys.length; i++) {
+          const key = keys[i]; // 각각의 키
+          const value = obj[key]; // 각각의 키에 해당하는 각각의 값
+          state.playerMap[value].voters = message.playerMap[value].voters;
+        }
+        // } else if (message.type === "REJOIN") {
+        //   state.gameStatus = message.gameStatus;
+        //   state.players = message.players;
+      } else {
+        console.log(
+          `sorry, unexpected message type. this is what we'v got ${message.type}`
+        );
       }
     }
 
     function onPersonalMessageReceived(payload) {
       const message = JSON.parse(payload.body);
-
-      if (message.type === "start") {
+      if (message.type === "ROLE") {
         state.role = message.role;
-        state.jobClient = state.stompClient.subscribe(`/sub/${state.role}`, onJobMessageReceived);
+        state.mafias = message.mafia;
+        state.jobClient = state.stompClient.subscribe(
+          `/sub/${state.mySessionId}/${state.role}`,
+          onJobMessageReceived
+        );
+        if (state.role === "observer") {
+          state.publisher.publishAudio(false);
+          state.publisher.publishVideo(false);
+        }
       }
     }
 
     function onJobMessageReceived(payload) {
       const message = JSON.parse(payload.body);
-      state.players = message.players;
+      if (state.role === 'mafia') {
+      const keys = Object.keys(state.playerMap);
+      for (let i = 0; i < keys.length; i++) {
+        const key = keys[i]; // 각각의 키
+        const value = obj[key]; // 각각의 키에 해당하는 각각의 값
+        state.playerMap[value].voter = message.playerMap[value].voter;
+      }
+      }  else if (state.role === 'police') {
+        let targetNickname = ''
+              for (let i = 0; i < state.subscribers.length; i++) {
+                if (state.subscribers[i].playerId === message.vote) {
+                  targetNickname = state.subscribers[i].nickname
+                  break
+                }
+              }
+              const targetJob = message.isMafia ? '마피아' : '시민'
+        state.submessage = `당신이 지목한 ${targetNickname}의 직업은 ${targetJob}입니다.`
+      }
+    }
+
+    function leaveGame() {
+      const message = {
+        type: "LEAVE",
+        id: state.playerId,
+      };
+      state.stompClient.send(
+        `/pub/${state.mySessionId}`,
+        {},
+        JSON.stringify(Message)
+      );
     }
 
     state.openviduToken = store.getters["token/getOpenviduToken"];
@@ -261,7 +478,8 @@ export default {
     joinSession();
     // connect();
 
-    // window.addEventListener("beforeunload", leaveSession);
+    window.addEventListener("beforeunload", leaveSession);
+    window.addEventListener("beforeunload", leaveGame);
     return {
       state,
       leaveSession,
