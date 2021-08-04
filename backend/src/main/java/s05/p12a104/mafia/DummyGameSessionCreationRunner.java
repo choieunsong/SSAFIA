f4f48ebce5eceaf16220c30c5c7d92cb0eead593
김지훈 <hoonti06@gmail.com>
2021-08-04 14:18:48 +0900

    feat : local 테스트 목적의 dummy gamesession 생성하는 application runner 추가

---
+++ backend/src/main/java/s05/p12a104/mafia/DummyGameSessionCreationRunner.java
@@ -0,0 +1,43 @@
+package s05.p12a104.mafia;
+
+import io.openvidu.java.client.OpenVidu;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import s05.p12a104.mafia.domain.dao.GameSessionDao;
+import s05.p12a104.mafia.domain.entity.AccessType;
+import s05.p12a104.mafia.domain.entity.GameState;
+import s05.p12a104.mafia.domain.entity.RoomType;
+import s05.p12a104.mafia.domain.repository.GameSessionRedisRepository;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+@Profile("default")
+public class DummyGameSessionCreationRunner implements ApplicationRunner {
+
+  private final GameSessionRedisRepository gameSessionRedisRepository;
+  private final OpenVidu openVidu;
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+    LocalDateTime createdTime = LocalDateTime.now();
+    GameSessionDao newGameSessionDao = GameSessionDao.builder()
+        .roomId("V1234").accessType(AccessType.PRIVATE).roomType(RoomType.BASIC)
+        .creatorEmail("dummy@dummy.com").createdTime(createdTime)
+        .finishedTime(createdTime)
+        .state(GameState.WAIT)
+        .sessionId(openVidu.createSession().getSessionId())
+        .playerMap(new HashMap<>())
+        .build();
+
+    GameSessionDao saved = gameSessionRedisRepository.save(newGameSessionDao);
+
+    log.info("Dummy Gamesession Created - roomId : {}", saved.getRoomId());
+  }
+}
