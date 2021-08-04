9d722d9d9201826d41f51dc0644d91b59b0c2b8c
adlguswjda <adlguswjda@naver.com>
2021-08-04 17:42:53 +0900

    style: 잘못 올린 코드 돌려 놓기

--- backend/src/main/java/s05/p12a104/mafia/domain/entity/GameRole.java
+++ backend/src/main/java/s05/p12a104/mafia/domain/entity/GameRole.java
@@ -1,16 +1,9 @@
 package s05.p12a104.mafia.domain.entity;
 
-import java.util.Random;
-
 public enum GameRole {
   CIVILIAN,
   MAFIA,
   DOCTOR,
   POLICE,
-  OBSERVER;
-  
-  public static GameRole getRandomRole() {
-    Random random = new Random();
-    return values()[random.nextInt(values().length-1)];
-  }
+  OBSERVER
 }
