45c39cad4abf6f3e610fb6e74fd4002bb62f2002
choieunsong <greatgelly96@gmail.com>
2021-08-18 17:49:00 +0900

    fix: 꽃가루 수정

--- frontend/src/views/game/Game.vue
+++ frontend/src/views/game/Game.vue
@@ -946,7 +946,7 @@ export default {
                             }
                         }
                         state.removeList = [];
-                        state.gameStatus = message.gameStatus
+                        state.gameStatus = message.gameStatus;
                         infoUpdater("isHost", message);
                         store.dispatch("ingame/setPhase", state.gameStatus.phase);
                         break;
@@ -1290,7 +1290,7 @@ export default {
                                 state.message = `게임이 종료되었습니다. <br> <span style="font-size:25px; color:${state.victimColor}">${victims}</span>가 사망하며 마피아를 모두 제거하였기 때문에 <span style="font-size: 25px; color:${state.civilColor}">시민측 진영</span>이 승리하였습니다.`;
                             }
                         }
-                        state.gameStatus = message.gamesTatus
+                        state.gameStatus = message.gamesTatus;
                         break;
                     }
                 }
@@ -1401,7 +1401,6 @@ export default {
                 ) {
                     sendMessageConfirm();
                 } else if (state.gameStatus.phase === "NIGHT_VOTE") {
-                    console.log("Night Vote Confirm");
                     sendMessageNightConfirm();
                 }
             }
