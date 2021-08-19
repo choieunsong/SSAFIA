9c42c8eefa1cd4f01251d35a89bccf5cef5b7aca
김지훈 <hoonti06@gmail.com>
2021-08-19 11:13:31 +0900

    feat : POST /api/gamesession/{roomId}의 response 항목에 color와 nickname 추가

--- backend/src/main/java/s05/p12a104/mafia/api/response/GameSessionJoinRes.java
+++ backend/src/main/java/s05/p12a104/mafia/api/response/GameSessionJoinRes.java
@@ -3,12 +3,25 @@ package s05.p12a104.mafia.api.response;
 import io.swagger.annotations.ApiModel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import s05.p12a104.mafia.domain.entity.Player;
+import s05.p12a104.mafia.domain.enums.Color;
 
 @ApiModel("GameSessionJoinResponse")
 @Getter
 @RequiredArgsConstructor
 public class GameSessionJoinRes {
+
   private final PlayerJoinRoomState state;
   private final String token;
   private final String playerId;
+  private final String nickname;
+  private final Color color;
+
+  public GameSessionJoinRes(PlayerJoinRoomState state, Player player) {
+    this.state = state;
+    this.token = player.getToken();
+    this.playerId = player.getId();
+    this.nickname = player.getNickname();
+    this.color = player.getColor();
+  }
 }
